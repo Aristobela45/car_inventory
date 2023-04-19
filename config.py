a5c09b133510a4d46aa1439ba6cfb7a6a8f1d443
Aristo Bela <aristo.bela13@gmail.com>
2023-04-18 23:31:09 -0700

    update

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
     set config
     """
-
+    FLASK_APP = os.environ.get('FLASK_APP')  
+    FLASK_ENV = os.environ.get('FLASK_ENV')  
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "Hello"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite://" + os.path.join(basedir, 'app.db')
